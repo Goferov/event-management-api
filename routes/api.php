c68c687e92fca80d8b452bf77716e77bcd6b16c9
Goferov <marcingodfryd@gmail.com>
2024-09-09 00:42:25 +0200

    Create mailpit in docker-compose to test mailing

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\AttendeeController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\EventController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
